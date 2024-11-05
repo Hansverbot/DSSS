5de79ba84f02acbe8748148804f45821711b8e2b
Hansverbot <geier.jan@outlook.de>
2024-11-05 13:49:44 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1.0',  # Passe die Versionsnummer an
+    description='Game to learn math',  # Füge eine Beschreibung hinzu
+    packages=find_packages(),
+    install_requires=[],  # Füge benötigte Pakete hinzu, falls vorhanden
+)
